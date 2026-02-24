c0cd10b5f14c381160664dafb07ac68a2582d872
test3html <detabasejob@gmail.com>
2026-02-25 01:38:15 +0900

    Create shorts取得.js

---
+++ おまけ/shorts取得.js
@@ -0,0 +1,55 @@
+(()=>{
+const videos=(function(){
+const contentClasses=document.querySelector("ytd-two-column-browse-results-renderer.style-scope.ytd-browse.grid.grid-3-columns").querySelector("#primary");
+const box=contentClasses.getElementsByClassName("style-scope ytd-two-column-browse-results-renderer")[0].querySelector("#contents");
+const shorts=box.getElementsByClassName("style-scope ytd-rich-grid-renderer");
+const getableShorts=Array.from(shorts).filter(item=>getComputedStyle(item).display!=="none");
+const links=getableShorts.map(item=>{
+const url=item.querySelector("#content.style-scope.ytd-rich-item-renderer").getElementsByClassName("shortsLockupViewModelHostEndpoint reel-item-endpoint")[0];
+return url.href;
+});
+if(links.find(item=>!item)){
+alert("リンクが取得できない箇所がありました");
+return;
+}
+if(links.length===0){
+alert("リンクが取得できない箇所がありました");
+return;
+}
+return links;
+})();
+const ids=(function(array){
+const customURLs=array.map(item=>{
+const ID=(function(ID){
+if(ID.includes("watch?v=")){
+const IdElement=ID.split("watch?v=");
+return IdElement[IdElement.length-1];
+}
+if(ID.includes("/shorts/")){
+const IdElement=ID.split("/shorts/");
+return IdElement[IdElement.length-1];
+}
+return;
+//ショート動画のURLでもそれ以外でもない場合はなし
+})(item);
+if(!ID){
+alert("適合するURLがありませんでした");
+return;
+}
+return ID;
+});
+return customURLs;
+})(urls);
+const txt=(function(list){
+const arrayText=list.join("\n")
+return arrayText;
+})(ids);
+const blob=new Blob([txt],{"type":"text/plain"});
+const aTag=docuemnt.createElement("a");
+const downLoadPath=URL.createObjectURL(blob);
+aTag.href=downLoadPath;
+aTag.downLoad="youtubeVideoList.txt";
+document.body.appendChild(aTag);
+aTag.click();
+URL.revokeObjectURL(downLoadPath);
+})();
