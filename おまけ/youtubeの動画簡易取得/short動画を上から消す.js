5c3eb53896a2c7b7830bfcc8955818886ba9ba31
test3html <detabasejob@gmail.com>
2026-03-01 13:58:21 +0900

    Create short動画を上から消す.js

---
+++ おまけ/youtubeの動画簡易取得/short動画を上から消す.js
@@ -0,0 +1,24 @@
+(()=>{
+const videos=(function(){
+const contentClasses=document.getElementsByClassName("style-scope ytd-rich-grid-renderer")["contents"];
+const lines=contentClasses.querySelectorAll("ytd-rich-item-renderer");
+const forcus=Array.from(lines).filter(el=>getComputedStyle(el).display!=="none");
+const number=Number(prompt("何件非表示にしますか?"));
+if(typeof number!=="number"){
+alert("数字を入力してください");
+return;
+}
+if(number<0){
+alert("整数で入力してください");
+return;
+}
+if(!Number.isInteger(number)){
+alert("整数で入力してください");
+return;
+}
+for(let i=0;i<number&&i<forcus.length;i++){
+if(forcus[i].style){
+forcus[i].style.display="none";
+}
+}
+})();
